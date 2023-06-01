e95ad1132b8b59839603b20eab170659b703b44b
Andrew Smiley <andrews@uriux.com>
2023-06-01 09:45:48 -0400

    fix: ambiguous export

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,3 @@
-import { Static, TSchema, Type } from '@sinclair/typebox';
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const removeUndefined = (obj: any) => {
     const stack = [obj];
@@ -14,8 +12,4 @@ export const removeUndefined = (obj: any) => {
         }
     }
     return obj;
-};
-
-export const PartialSome = <T extends TSchema, K extends (keyof Static<T>)[]>(schema: T, keys: readonly [...K]) => {
-    return Type.Intersect([Type.Omit(schema, keys), Type.Partial(Type.Pick(schema, keys))]);
 };
